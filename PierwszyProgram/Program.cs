c39cdbd5887586429fcf40f90f064a03e7e98999
krzysztof <krzysztof.melerqa@gmail.com>
2026-03-28 20:24:20 +0100

    pierwsdzy Program

---
+++ PierwszyProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string szkola = "Szkoła programowania";
+        Console.WriteLine($"Hello, World! Witaj w {szkola}!");
+        Console.WriteLine("jak masz na imie?");
+        string? imie = Console.ReadLine();
+        Console.WriteLine($"Cześć buraku, {imie}!");
+    }
+}
